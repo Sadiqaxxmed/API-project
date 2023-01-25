5ad783086a76a0a1a34bbbc8ba57b030cffddad8
Sadiq Ahmed <43020644+Sadiqaxxmed@users.noreply.github.com>
2023-01-24 22:57:07 -0800

    Finished with review table

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -13,6 +13,8 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Spot.belongsTo(models.User, { foreignKey: 'ownerId'});
       Spot.hasMany(models.Booking, { foreignKey: 'spotId', onDelete: "CASCADE"});
+      Spot.hasMany(models.Review, { foreignKey: 'spotId', onDelete: "CASCADE"});
+
     }
   }
 
