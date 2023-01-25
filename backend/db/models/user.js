5013f711fbdb6e6d6dfe19077793ee8e81770cc0
Sadiq Ahmed <43020644+Sadiqaxxmed@users.noreply.github.com>
2023-01-24 21:14:32 -0800

    Create Spot table

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -48,6 +48,8 @@ module.exports = (sequelize, DataTypes) => {
 
     static associate(models) {
       // define association here
+      User.hasMany(models.Spot, { foreignKey: 'ownerId', onDelete: "CASCADE" })
+
     }
   };
 
