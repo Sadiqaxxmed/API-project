e90501748b48ad7a2287a75f9f5021b13d1c85e6
Sadiq Ahmed <43020644+Sadiqaxxmed@users.noreply.github.com>
2023-01-25 10:55:44 -0800

    Done updating getCurrUsers

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -42,6 +42,29 @@ router.post(
   }
 );
 
+
+// Log out
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
+// router.get(
+//   '/',
+//   restoreUser,
+//   (req, res) => {
+//     const { user } = req;
+//     if (user) {
+//       return res.json({
+//         user: user.toSafeObject()
+//       });
+//     } else return res.json({ user: null });
+//   }
+// );
+
 //Get current user
 
 router.get('/',
@@ -68,26 +91,5 @@ restoreUser,
   }
 });
 
-// Log out
-router.delete(
-  '/',
-  (_req, res) => {
-    res.clearCookie('token');
-    return res.json({ message: 'success' });
-  }
-);
-
-router.get(
-  '/',
-  restoreUser,
-  (req, res) => {
-    const { user } = req;
-    if (user) {
-      return res.json({
-        user: user.toSafeObject()
-      });
-    } else return res.json({ user: null });
-  }
-);
 
 module.exports = router;
