9a3946cec3c02796f08272f7cfcfc3a16438c042
Sadiq Ahmed <43020644+Sadiqaxxmed@users.noreply.github.com>
2023-01-26 15:42:57 -0800

    editReview

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,6 +7,49 @@ const { handleValidationErrors, validateReview, validateReviewImage } = require(
 const sequelize = require('sequelize');
 const { reviewExists, usersReview } = require('../../utils/error-handles')
 
+//Edit a Review
+router.put('/:reviewId', requireAuth, validateReview, reviewExists, usersReview, async (req, res, next) => {
+  const { reviewId } = req.params;
+  const { review, stars } = req.body;
+  const user = req.user;
+
+  let editReview = await Review.findByPk(reviewId);
+
+  editReview.review = review;
+  editReview.stars = stars;
+
+  await editReview.save();
+
+  return res.json(editReview);
+})
+
+// router.put('/:reviewId',
+//   requireAuth,
+//   validateReview,
+//   async (req, res, next) => {
+//     try {
+//         const { reviewId } = req.params;
+//         const { review, stars } = req.body;
+//         const user = req.user;
+
+//         // Check if review exists and belongs to user
+//         const reviewToEdit = await Review.findOne({
+//           where: { id: reviewId, userId: user.id }
+//         });
+//         if (!reviewToEdit) {
+//           return res.status(404).json({ message: "Review not found or not belongs to user" });
+//         }
+
+//         // Update review
+//         reviewToEdit.review = review;
+//         reviewToEdit.stars = stars;
+//         await reviewToEdit.save();
+
+//         return res.json(reviewToEdit);
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 
 //Delete review
 router.delete("/:reviewId", requireAuth, reviewExists, usersReview, async (req, res) => {
