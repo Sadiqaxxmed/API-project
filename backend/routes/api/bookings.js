e936e33f5b5c5057b0543367adb6557fc75884be
Sadiq Ahmed <43020644+Sadiqaxxmed@users.noreply.github.com>
2023-01-27 14:13:20 -0800

    Done delete books

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const { User, Spot, Review, SpotImage, ReviewImage, Booking } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors, validateBooking } = require('../../utils/validation');
@@ -63,5 +63,49 @@ router.get("/current", requireAuth, async (req, res, next) => {
 });
 
 
+router.delete('/:bookingId', restoreUser, requireAuth, async (req, res, next) => {
+
+  const { user } = req
+
+  const bookingId = req.params.bookingId
+
+  if(!bookingId || bookingId === 'null'){
+      return res.status(404).json({
+          message: "Booking couldn't be found",
+          statusCode: 404
+      })
+  }
+
+  const findBooking = await Booking.findByPk(bookingId)
+
+  if(!findBooking){
+      return res.status(404).json({
+          message: "Booking couldn't be found",
+          statusCode: 404
+      })
+  }
+
+  if(findBooking.dataValues.userId !== user.id){
+      return res.status(400).json({
+          message: "Validation error",
+          statusCode: 400
+      })
+  }
+
+  if(new Date(findBooking.dataValues.startDate) <= new Date()){
+      return res.status(403).json({
+          message: "Bookings that have been started can't be deleted",
+          statusCode: 403
+      })
+  }
+
+  await findBooking.destroy()
+
+  return res.status(200).json({
+      message: "Successfully deleted",
+      statusCode: 200
+  })
+
+})
 
 module.exports = router;
